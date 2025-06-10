621213e8563ba47cb1426daa5f4750f782415e37
Alexis Herrera <desert5@outlook.es>
2025-06-10 17:45:49 -0400

    Refactor figura classes to improve code readability by removing unnecessary whitespace

--- src/app/models/figuras-geometricas.ts
+++ src/app/models/figuras-geometricas.ts
@@ -1,63 +1,50 @@
 // Classe base
-
 export abstract class GeometricFigure {
   protected name: string;
   constructor(name: string) {
     this.name = name;
   }
-
   abstract calculatePerimeter(): number;
-
   getName(): string {
     return this.name;
   }
 }
-
 // Classe derivada: Círculo
 export class Circle extends GeometricFigure {
   private radius: number;
-
   constructor(radius: number) {
     super('Círculo');
     this.radius = radius;
   }
-
   calculatePerimeter(): number {
     return 2 * Math.PI * this.radius;
   }
-
   getRadius(): number {
     return this.radius;
   }
 }
-
 // Classe derivada: Triangulo Escaleno
 export class Triangle extends GeometricFigure {
   private sideA: number;
   private sideB: number;
   private sideC: number;
-
   constructor(sideA: number, sideB: number, sideC: number) {
     super('Triângulo Escaleno');
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
   }
-
   calculatePerimeter(): number {
     return this.sideA + this.sideB + this.sideC;
   }
-
   getSides(): [number, number, number] {
     return [this.sideA, this.sideB, this.sideC];
   }
 }
-
 // Classe derivada: Triangulo Equilátero
 export class EquilateralTriangle extends Triangle {
   constructor(side: number) {
     super(side, side, side);
     this['name'] = 'Triangulo Equilátero';
   }
-
 }
